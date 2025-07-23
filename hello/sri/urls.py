996ffae117b6321a9ec5b9add238a938d03b8326
vishnu-28g <vishnuvishnu50097@gmail.com>
2025-07-23 11:29:43 +0530

    first commit

---
+++ hello/sri/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',home , name='home'),# API - POSTMAN
+    path('index/', index, name='index'),
+    path('demo/',demo, name='demo'),
+]
